d0a08f10416fc0752fa9f26935a982daa897ce09
dinosoupy <anishbasoup@gmail.com>
2023-04-25 15:11:51 +1000

    Final submission --accepted

--- add-two-numbers/main.go
+++ add-two-numbers/main.go
@@ -5,87 +5,69 @@ import (
 )
 
 type ListNode struct {
-  Val int
-  Next *ListNode
-}
-
-type List struct {
-	Head *ListNode
+	Val  int
+	Next *ListNode
 }
 
 func main() {
-	arr1 := [3]int{3,4,2}
-	arr2 := [3]int{7,5,6}
+	arr1 := []int{9,9}
+	arr2 := []int{9}
 
-	l1 := List{}
-	for i := 0; i < 3; i++ {
-		l1.Insert(arr1[i])
-	}
-	fmt.Printf("This is the first List: ")
-	Show(&l1)
+	l1 := toListNode(arr1)
+	l2 := toListNode(arr2)
 
-	l2 := List{}
-	for i := 0; i < 3; i++ {
-		l2.Insert(arr2[i])
-	}
-	fmt.Printf("This is the second List: ")
-	Show(&l2)
-
-	resultHead := addTwoNumbers(l1.Head,l2.Head)
-	fmt.Printf("This is the resulting List: ")
-	for resultHead.Next!=nil {
+	resultHead := addTwoNumbers(l1, l2)
+	fmt.Print("This is the resulting List: ")
+	for resultHead != nil {
 		fmt.Print(resultHead.Val)
 		resultHead = resultHead.Next
 	}
-
+	fmt.Println()
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	// Make a new ListNode and store its first element in head
-	head := new(ListNode)
-	// Make an interator element for traversing ListNodes (at first, it's head)
-	iter := head
+	dummyHead := new(ListNode) // Make a new ListNode head for the output
+	iter := dummyHead // Make an iterator element for traversing ListNodes (at first, it's head)
+
 	carry := 0
-	// Start iterating if either l1 or l2 is non-empty or if there's a carry
-	if l1 != nil || l2 != nil || carry != 0 {
-		// Assign 0 to nil nodes
-		if l1 == nil {
-			l1.Val = 0
+	// Start iterating if either l1 or l2 is non-empty
+	for l1 != nil || l2 != nil {
+		// Assign 0 to nil nodes in case one of the ListNodes is longer
+		if l1 == nil && l2 != nil {
+			l1 = &ListNode{Val: 0}
 		}
-		if l2 == nil {
-			l2.Val = 0
+		if l2 == nil && l1 != nil {
+			l2 = &ListNode{Val: 0}
 		}
 
-		fmt.Printf("l1 = \nl2 = \n", l1.Val, l2.Val)
-		// Add values and determine carry
-		sum := carry + l1.Val + l2.Val
-		iter.Next = &ListNode{Val: sum % 10, Next: nil}
+		// Add and set values; determine carry
+		sum := l1.Val + l2.Val + carry
+		iter.Next = &ListNode{Val: sum % 10}
 		carry = sum / 10
 
-		// Advance l1, l2
+		// Advance l1, l2, and iter
 		l1 = l1.Next
 		l2 = l2.Next
+		iter = iter.Next
 	}
-	return head
+	// If there is a trailing carry, add that to the end
+	if carry > 0 {
+		iter.Next = &ListNode{Val: carry}
+	}
+	return dummyHead.Next
 }
 
-func (l *List) Insert(v int) {
-	list := &ListNode{Val: v, Next: nil}
-	if l.Head == nil {
-		l.Head = list
-	} else {
-		p := l.Head
-		for p.Next != nil {
-			p = p.Next
-		}
-		p.Next = list
+// Helper function to convert arrays into ListNode for testing
+func toListNode(arr []int) *ListNode {
+	// Make a dummy head and iterator
+	dummyHead := new(ListNode)
+	iter := dummyHead
+
+	// Loop through array items and add them as nodes in ListNode
+	for i := 0; i < len(arr); i++ {
+		iter.Next = &ListNode{Val: arr[i]}
+		iter = iter.Next
 	}
+	return dummyHead.Next
 }
 
-func Show(l *List) {
-	p := l.Head
-	for p != nil {
-		fmt.Printf("-> %v ", p.Val)
-		p = p.Next
-	}
-}
