8ce5aa13212a8d2dcf044a46074a489f8b367698
dinosoupy <anishbasoup@gmail.com>
2023-05-02 12:18:29 +1000

    Final submission --accepted --notes:runtime could be optimized

--- car-fleet/main.go
+++ car-fleet/main.go
@@ -6,51 +6,37 @@ import (
 )
 
 func main() {
-	pos := []int{0,2,4}
-	speed := []int{4,2,1}
-	target := 100
+	pos := []int{10,8,0,5,3}
+	speed := []int{2,4,1,1,3}
+	target := 12
 	fmt.Println(carFleet(target, pos, speed))
 }
 
 func carFleet(target int, position []int, speed []int) int {
-	// Create variable n for ease of use
-	n := len(position)
-	// Make a map[position]speed of length n (where n = number of cars)
-  m := make(map[int]int, n)
-  // Sort speed array
-  speedCopy := speed
-  sort.Ints(speedCopy)
-  fleets := 0 // Start with 0 fleets
-  // Populate map with 0
-  for k := 0; k < target+1; k++ {
-  	m[k] = 0
-  }
+	// Array of cars with (position, speed) as elements
+	cars := make([][2]int, len(position))
 
-  // keep looping until speed of the last car to reach target is the slowest speed amongst all cars
-  for m[target] != speedCopy[0] {
-  	// Iterate through all n cars
-  	for i := 0; i < n; i++ {
-  		// Increment position by speed, this is one cycle
-  		position[i] += speed[i]
-  		// The map will store the least possible speed for a particular position 
-  		// If we encounter a speed greater than the speed stored for a position, that means a faster car has encountered
-  		// a slower one in its same position (bumper to bumper) and has therefore coalesced into a fleet
-  		// At that point we can change the speed of that position to the lowest speed of the two
-  		currentSpeed := m[position[i]]
-  		// My speed greater than car already in this position
-			if speed[i] > currentSpeed {
-				speed[i] = currentSpeed
-  			m[position[i]] = speed[i]
-  			// Increase fleet count by 1
-   			fleets += 1
-  			
-  		} else {
-  			// Increment positions for the cars that didnt coalesce into fleets
-				m[position[i]] = speed[i]
-				position[i] += speed[i]
-  		}
-		} 
-		m[position[i]] = speed[i]
-  }
-  return fleets
+	// Parse positions and speed into cars
+	for i := 0; i < len(position); i++ {
+		cars[i] = [2]int{position[i], speed[i]}
+	}
+
+	// Sort cars by desc order of position (closest to target first)
+	sort.Slice(cars, func(i, j int) bool {
+		return cars[i][0] > cars[j][0]
+	})
+
+	var currentSlowest float64
+	var fleet int
+
+	for i := 0; i < len(position); i++ {
+		timeToTarget := float64(target - cars[i][0])/ float64(cars[i][1])
+		// Loop through each cars timeToTarget; since timeToTarget should be monotonic non-decreasing for the cars array
+		// any violation is counted as a fleet being added
+		if timeToTarget > currentSlowest {
+			currentSlowest = timeToTarget
+			fleet ++
+		}
+	}
+	return fleet
 }
