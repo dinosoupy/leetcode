c6403cc94123eabc8d1ea71e1a46864fa20a0730
dinosoupy <anishbasoup@gmail.com>
2023-08-24 22:30:15 +1000

    added clean dfs solution for clone graph

--- clone-graph/main.go
+++ clone-graph/main.go
@@ -6,68 +6,29 @@
  * }
  */
 
-func cloneGraph(node *Node) *Node {
-    
-}
-
-// dfs solution
 
 func cloneGraph(node *Node) *Node {
-    if node == nil {
-        return nil
-    }
-    
-    record := make(map[*Node]*Node)
-    
-    dfs(node, record)
-    
-    return record[node]
+    // If graph is empty
+   if node==nil{
+       return node
+   }
+   visited:=make(map[*Node]*Node)
+   dfs(node, visited)
+   return visited[node]
 }
 
-func dfs(node *Node, record map[*Node]*Node) {
-    if node == nil {
-        return 
+func dfs(node *Node, visited map[*Node]*Node) {
+    // Recursion stopping condition
+    if node==nil {
+        return
     }
-    
-    newNode := new(Node)
-    newNode.Val = node.Val
-    
-    record[node] = newNode
-    
-    for _, child := range node.Neighbors {
-        if _, exist := record[child]; !exist {
-            dfs(child, record)
-        }
-        
-        newNode.Neighbors = append(newNode.Neighbors, record[child])
+    newNode:=&Node{Val: node.Val}
+    visited[node]=newNode
+    for _, neighbor:= range node.Neighbors {
+        if _, ok:=visited[neighbor]; !ok {
+            dfs(neighbor, visited)
+        } 
+        newNode.Neighbors = append(newNode.Neighbors, visited[neighbor])
     }
-}
-
-
-
-// Cache solution
-
-func cloneGraph(node *Node) *Node {
-    cache := map[*Node]*Node{}
-    return cloneGraph2(node, cache)
-}
-
-func cloneGraph2(node *Node, cache map[*Node]*Node) *Node {
-    if node == nil {
-        return nil
-    }
-    if n, ok := cache[node]; ok {
-        return n
-    }
-    clone := &Node{Val: node.Val, Neighbors: make([]*Node, 0)}
-    cache[node] = clone
-    for i := 0; i < len(node.Neighbors); i++ {
-        n := cloneGraph2(node.Neighbors[i], cache)
-        if n != nil {
-            clone.Neighbors = append(clone.Neighbors, n)
-        }
-    }
-    return clone
-
 }
 
